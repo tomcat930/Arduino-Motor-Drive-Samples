c8d59d947a9b6240884a68d1fc0f819c747f76c0
Tomohiro Murayama <silhouette.formula1@gmail.com>
2023-08-13 15:35:31 +0900

    Add comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 
-const int IN1_pin = 9;   // 9番ピンを出力ピンIN1と定義
-const int IN2_pin = 8;   // 8番ピンを出力ピンIN2と定義
-const int ENA_pin = 10;  // 10番ピンを出力ピンENAと定義
-
+const int IN1_pin = 9;      // 9番ピンを出力ピンIN1と定義
+const int IN2_pin = 8;      // 8番ピンを出力ピンIN2と定義
+const int ENA_pin = 10;     // 10番ピンを出力ピンENAと定義
 const int lmSW_L_pin = 12;  // 11番ピンをLMスイッチ1と定義
 const int lmSW_R_pin = 11;  // 12番ピンをLMスイッチ2と定義
-
 const int mainSW_pin = 7;
 
 bool lmSW_L_mode = false;
@@ -17,16 +15,16 @@ bool mainSW_status = false;
 int mode = 0;
 int old_mode;
 int drive_ready = 0;
-int motor_speed = 255;
+int motor_speed = 255;  // モーター速度
 
 const int stop_mode = 0;
 const int forward_mode = 1;
 const int back_mode = 2;
 
-unsigned long startTime;
-unsigned long currentTime;
-unsigned long operationTime;
-const unsigned long period = 5000;  // 動作継続時間(秒)
+unsigned long start_time;                         // 動作開始時間
+unsigned long current_time;                       // 現在時間
+unsigned long operation_time;                     // 動作時間
+const unsigned long operation_limit_time = 5000;  // 動作継続時間(秒)
 
 void setup() {
   Serial.begin(115200);
@@ -47,30 +45,28 @@ void motorStop() {
   digitalWrite(IN2_pin, LOW);
 }
 
-void forwardDrive() {
-  digitalWrite(IN1_pin, HIGH);
-  digitalWrite(IN2_pin, LOW);
-  analogWrite(ENA_pin, motor_speed);
-}
-
-void backDrive() {
-  digitalWrite(IN1_pin, LOW);
-  digitalWrite(IN2_pin, HIGH);
-  analogWrite(ENA_pin, motor_speed);
-}
-
+/**
+ * @brief 動作継続時間を超えた場合停止する
+ *
+ */
 void safetyTimerStop() {
-  if (startTime == 0) {
-    startTime = currentTime;
+  if (start_time == 0) {
+    start_time = current_time;
   }
-  if (currentTime >= startTime + period) {
+  if (current_time >= start_time + operation_limit_time) {
     Serial.println("Safety stopped.");
     drive_ready = 0;
-    startTime = 0;
-    mode = 0;
+    start_time = 0;
+    mode = stop_mode;
   }
 }
 
+/**
+ * @brief モーターを動作させる
+ *
+ * @param IN1 モーターのIN1
+ * @param IN2 モーターのIN2
+ */
 void motorDrive(int IN1, int IN2) {
   safetyTimerStop();
   digitalWrite(IN1_pin, IN1);
@@ -79,7 +75,7 @@ void motorDrive(int IN1, int IN2) {
 }
 
 void loop() {
-  currentTime = millis();
+  current_time = millis();
   lmSW_L_mode = !digitalRead(lmSW_L_pin);
   lmSW_R_mode = !digitalRead(lmSW_R_pin);
   mainSW_mode = !digitalRead(mainSW_pin);
@@ -100,7 +96,7 @@ void loop() {
     }
 
     if (mode != old_mode) {
-      startTime = 0;
+      start_time = 0;
     }
 
     switch (mode) {
